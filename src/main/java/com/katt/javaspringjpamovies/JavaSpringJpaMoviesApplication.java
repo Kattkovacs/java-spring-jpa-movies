6f9fbf9f1531378fd5edddbe83c6de6278ea5e9e
Kattkovacs <kattkovacs@gmail.com>
2020-11-10 17:15:38 +0100

    Initial commit

---
+++ src/main/java/com/katt/javaspringjpamovies/JavaSpringJpaMoviesApplication.java
@@ -0,0 +1,13 @@
+package com.katt.javaspringjpamovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringJpaMoviesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringJpaMoviesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/katt/javaspringjpamovies/JavaSpringJpaMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.katt.javaspringjpamovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringJpaMoviesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
